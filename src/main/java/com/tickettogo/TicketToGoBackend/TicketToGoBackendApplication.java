c329e44cabf5b6e0a01082db8300b50d324b0c92
JESSRIEL JAMILLA <jamillajessriel17@gmail.com>
2023-09-02 12:57:08 +0800

    init project

---
+++ src/main/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tickettogo.TicketToGoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketToGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketToGoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tickettogo.TicketToGoBackend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketToGoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
