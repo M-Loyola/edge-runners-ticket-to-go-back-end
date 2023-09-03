06a1cb6a48466ea62aa0ca1013d5cb38daf48397
Jens Jovellano <jensjovellano@gmail.com>
2023-09-03 11:35:53 +0800

    Feat: Added cross origin for front-end api call

--- src/main/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplication.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplication.java
@@ -3,11 +3,12 @@ package com.tickettogo.TicketToGoBackend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "ex-ticket-to-go-front-end-qa.up.railway.app")
 @SpringBootApplication
 @RestController
 public class TicketToGoBackendApplication {
