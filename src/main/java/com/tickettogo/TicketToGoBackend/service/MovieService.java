ac11003e4b2304abc83a6946c70a262a25cbeed0
Jens Jovellano <jensjovellano@gmail.com>
2023-09-04 14:32:27 +0800

    Feat: Created MovieService with getAll function call.

---
+++ src/main/java/com/tickettogo/TicketToGoBackend/service/MovieService.java
@@ -0,0 +1,16 @@
+package com.tickettogo.TicketToGoBackend.service;
+
+import com.tickettogo.TicketToGoBackend.entity.Movie;
+import com.tickettogo.TicketToGoBackend.repository.MoviesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class MovieService {
+    @Autowired
+    MoviesRepository moviesRepository;
+    public List<Movie> getAllMovies() {
+        return moviesRepository.findAll();
+    }
+}
