5a401ce05384f6cdd0bcd6b34472d3f5ae8ee74c
jamillajessriel17 <76465232+jamillajessriel17@users.noreply.github.com>
2023-09-04 14:50:19 +0800

    fix: remove movie list property in Cinema

--- src/main/java/com/tickettogo/TicketToGoBackend/entity/Cinema.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/entity/Cinema.java
@@ -3,8 +3,10 @@ package com.tickettogo.TicketToGoBackend.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Getter
@@ -15,11 +17,4 @@ public class Cinema {
     private Integer id;
     private String name;
     private String location;
-
-    @ManyToMany
-    @JoinTable(
-            name = "cinema_movies",
-            joinColumns = @JoinColumn(name = "cinema_id"),
-            inverseJoinColumns = @JoinColumn(name = "movie_id"))
-    List<Movie> movies;
 }
