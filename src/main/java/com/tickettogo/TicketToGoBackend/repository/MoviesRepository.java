5e691b390fb2fd02962a7d2c65e3c9a6211a90b1
Jens Jovellano <jensjovellano@gmail.com>
2023-09-04 14:32:44 +0800

    Feat: Created MoviesRepository

--- src/main/java/com/tickettogo/TicketToGoBackend/repository/MoviesRepository.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/repository/MoviesRepository.java
@@ -1,5 +1,6 @@
-package com.tickettogo.TicketToGoBackend;
+package com.tickettogo.TicketToGoBackend.repository;
 
+import com.tickettogo.TicketToGoBackend.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
