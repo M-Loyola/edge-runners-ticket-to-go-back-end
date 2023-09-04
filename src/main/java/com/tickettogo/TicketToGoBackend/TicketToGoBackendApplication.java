cc1d164d0d6a940f9e3799df4bea8b2a3af52b29
Jens Jovellano <jensjovellano@gmail.com>
2023-09-04 14:35:00 +0800

    Feat, Refactor: Removed test api calls in the main application.

--- src/main/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplication.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/TicketToGoBackendApplication.java
@@ -1,34 +1,15 @@
 package com.tickettogo.TicketToGoBackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 @SpringBootApplication
 @RestController
 public class TicketToGoBackendApplication {
 
-	@Autowired
-	private CinemaRepository cinemaRepository;
-
-	@Autowired
-	private MoviesRepository moviesRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(TicketToGoBackendApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public List<Cinema> getAll(){
-		return cinemaRepository.findAll();
-	}
-	@GetMapping("/movies")
-	public List<Movie> getAllMovies(){
-		return moviesRepository.findAll();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketToGoBackendApplication.class, args);
+    }
 
 }
