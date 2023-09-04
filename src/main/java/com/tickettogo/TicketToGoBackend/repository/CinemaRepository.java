17e10d0efb2b65b9f716613d45a647d274323975
Jens Jovellano <jensjovellano@gmail.com>
2023-09-04 14:34:26 +0800

    Feat, Refactor: Moved MovieRepository folder into organized package.

--- src/main/java/com/tickettogo/TicketToGoBackend/repository/CinemaRepository.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/repository/CinemaRepository.java
@@ -1,5 +1,6 @@
-package com.tickettogo.TicketToGoBackend;
+package com.tickettogo.TicketToGoBackend.repository;
 
+import com.tickettogo.TicketToGoBackend.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
