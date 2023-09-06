a1cff31c7492dac89412fe7775debc6588a3a7e1
Jens Jovellano <jensjovellano@gmail.com>
2023-09-06 15:23:13 +0800

    Refactor: Changed table names and columns in flyway

--- src/main/resources/db/migration/V20230904145048__details_cinema_movie_db.sql
+++ src/main/resources/db/migration/V20230904145048__details_cinema_movie_db.sql
@@ -1,9 +1,10 @@
-create table if not exists cinema_movie (
+create table if not exists details_cinema_movie (
     cinema_movie_id  int auto_increment primary key,
     movie_id int NOT NULL,
     cinema_id int NOT NULL,
     price int NOT NULL,
     schedule varchar(255) NULL,
+    occupied_seats varchar(1000) NULL,
     FOREIGN KEY (movie_id)
     REFERENCES movie(id),
 	FOREIGN KEY (cinema_id)
