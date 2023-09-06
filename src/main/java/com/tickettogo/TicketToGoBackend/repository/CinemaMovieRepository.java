52d389d79089e1a3812dbc868dca00746da9363d
Jens Jovellano <jensjovellano@gmail.com>
2023-09-06 15:21:49 +0800

    Refactor: Changed name of entity

--- src/main/java/com/tickettogo/TicketToGoBackend/repository/CinemaMovieRepository.java
+++ src/main/java/com/tickettogo/TicketToGoBackend/repository/CinemaMovieRepository.java
@@ -1,9 +1,9 @@
 package com.tickettogo.TicketToGoBackend.repository;
 
-import com.tickettogo.TicketToGoBackend.entity.CinemaMovie;
+import com.tickettogo.TicketToGoBackend.entity.DetailsCinemaMovie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CinemaMovieRepository extends JpaRepository<CinemaMovie, Integer> {
+public interface CinemaMovieRepository extends JpaRepository<DetailsCinemaMovie, Integer> {
 }
