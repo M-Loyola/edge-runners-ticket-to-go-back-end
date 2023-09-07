3a32c7ab2488789625bd608220257c6514e5a5f0
jamillajessriel17 <76465232+jamillajessriel17@users.noreply.github.com>
2023-09-08 02:44:16 +0800

    chore: flyway initial data for prod

--- src/main/resources/db/migration/V20230904144553__movie_db.sql
+++ src/main/resources/db/migration/V20230904144553__movie_db.sql
@@ -2,7 +2,7 @@ create table if not exists movie (
     id  int auto_increment primary key,
     title varchar(255) NOT NULL,
     duration int NOT NULL,
-    description varchar(255) NOT NULL,
+    description varchar(1000) NOT NULL,
     image varchar(255) NOT NULL,
     is_showing boolean NOT NULL
 );
--- src/main/resources/db/migration/V20230905190428__intial_data.sql
+++ src/main/resources/db/migration/V20230905190428__intial_data.sql
@@ -1,62 +1,284 @@
-insert into cinema(name, location )
-values ("Sm Sta rosa Cinema 1", "Laguna");
+INSERT INTO cinema(name, location) VALUES("Cinematheque Centre", "Manila");
+INSERT INTO cinema(name, location) VALUES("Robinsons Movieworld", "Manila");
+INSERT INTO cinema(name, location) VALUES("Times Theater", "Manila");
 
-insert into cinema(name, location )
-values ("Sm Sta rosa Cinema 2", "Laguna");
+INSERT INTO cinema(name, location) VALUES("The Podium Cinema", "Ortigas");
+INSERT INTO cinema(name, location) VALUES("Ortigas Cinema Estancia", "Ortigas");
+INSERT INTO cinema(name, location) VALUES("Red Carpet Cinemas", "Ortigas");
 
-insert into cinema(name, location )
-values ("Sm Ortigas Cinema 1", "Ortigas");
+INSERT INTO cinema(name, location) VALUES("Sunstar Cinema", "Laguna");
+INSERT INTO cinema(name, location) VALUES("Vista Cinemas", "Laguna");
+INSERT INTO cinema(name, location) VALUES("Festival Gold Class", "Laguna");
 
-insert into cinema(name, location )
-values ("Sm Ortigas Cinema 2", "Ortigas");
 
-insert into cinema(name, location )
-values ("Sm manila Cinema 1", "Manila");
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Tangled", 100, "The magically long-haired Rapunzel has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.", "https://m.media-amazon.com/images/M/MV5BMTAxNDYxMjg0MjNeQTJeQWpwZ15BbWU3MDcyNTk2OTM@._V1_FMjpg_UX1000_.jpg", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Transformers: Dark of the Moon", 154, "The Autobots learn of a Cybertronian spacecraft hidden on the moon, and race against the Decepticons to reach it and to learn its secrets.", "https://i.ebayimg.com/images/g/BcEAAOSwl9BkaOEj/s-l1200.webp", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Requiem for a Dream", 102, "The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.", "https://m.media-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg", false);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Shrek Forever After", 93, "Rumpelstiltskin tricks a mid-life crisis burdened Shrek into allowing himself to be erased from existence and cast in a dark alternate timeline where Rumpelstiltskin rules supreme.", "https://m.media-amazon.com/images/I/718yIvZosHL.jpg", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Cyberpunk: Edgerunners", 101, "A Street Kid trying to survive in a technology and body modification-obsessed city of the future. Having everything to lose, he chooses to stay alive by becoming an Edgerunner, a Mercenary outlaw also known as a Cyberpunk.", "https://m.media-amazon.com/images/M/MV5BMTQ0MGI4NTgtZTlkNy00YTU5LThjM2ItNDczMGFlZWY2ZWM3XkEyXkFqcGdeQXVyMTM3NDc1OTM2._V1_FMjpg_UX1000_.jpg", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Spider-Man 2", 127, "Peter Parker is beset with troubles in his failing personal life as he battles a brilliant scientist named Doctor Otto Octavius.", "https://m.media-amazon.com/images/M/MV5BMzY2ODk4NmUtOTVmNi00ZTdkLTlmOWYtMmE2OWVhNTU2OTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("Kung Fu Panda", 100, "To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.", "https://m.media-amazon.com/images/M/MV5BODJkZTZhMWItMDI3Yy00ZWZlLTk4NjQtOTI1ZjU5NjBjZTVjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg", true);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("One Piece Film: Red", 97, "For the first time ever, Uta - the most beloved singer in the world - will reveal herself to the world at a live concert. The voice that the whole world has been waiting for is about to resound.", "https://m.media-amazon.com/images/M/MV5BODY4OWM5M2UtM2Y1Yi00YjAyLTlhMDktMDkzZjFmMjI5MmI5XkEyXkFqcGdeQXVyMTA1NjQyNjkw._V1_FMjpg_UX1000_.jpg", false);
+INSERT INTO movie(title, duration, description, image, is_showing) VALUES("500 Days of Summer", 89, "After being dumped by the girl he believes to be his soulmate, hopeless romantic Tom Hansen reflects on their relationship to try and figure out where things went wrong and how he can win her back.", "https://m.media-amazon.com/images/M/MV5BMTk5MjM4OTU1OV5BMl5BanBnXkFtZTcwODkzNDIzMw@@._V1_FMjpg_UX1000_.jpg", true);
 
-insert into cinema(name, location )
-values ("Sm manila Cinema 2", "Manila");
-
-insert into cinema(name, location )
-values ("Taguig cinema", "Taguig");
 insert into movie(title, duration, description, image, is_showing )
-values ("John Wick", 140, "Lorem ipsum just john wick things", "https://sg-test-11.slatic.net/p/48f11fbde5fc8191bbfce7b5985633e6.jpg", true );
-insert into movie(title, duration, description, image,is_showing)
-values ("spider man", 150,"Lorem ipsum just spider man things", "https://m.media-amazon.com/images/I/815IBWT+WeL._AC_UF1000,1000_QL80_.jpg",true);
-insert into movie(title, duration, description, image,is_showing )
-values ("Forrest Gump", 160,"Lorem ipsum just kung fu panda  things", "https://flxt.tmsimg.com/assets/p15829_v_v13_aa.jpg",true);
-insert into movie(title, duration, description, image,is_showing )
-values ("Insidious", 160,"Lorem ipsum just Insidious  things", "https://miro.medium.com/v2/resize:fit:500/1*I9zDkUL5PfD7aT0O2Zk8mg.jpeg",true);
 
-insert into movie(title, duration, description, image,is_showing )
-values ("BumbleeBee", 160,"Lorem ipsum just kung fu panda 5  things", "https://m.media-amazon.com/images/I/71LlMXBogbL.jpg",true);
+values ("The Texas Chain Saw Massacre (1974)",
 
-insert into movie(title, duration, description, image,is_showing )
-values ("Home Alone", 160,"Lorem ipsum just kung fu panda 5  things", "https://image.tmdb.org/t/p/original/onTSipZ8R3bliBdKfPtsDuHTdlL.jpg",false);
+180,
 
-insert into movie(title, duration, description, image,is_showing )
-values ("Hypnotic", 160,"Lorem ipsum hypnotic things", "https://m.media-amazon.com/images/M/MV5BNmNmNWNiMjUtZmYyMC00OWVhLThmMTItNjkxYzU4Yzk2NTdlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg",false);
-insert into movie(title, duration, description, image,is_showing )
-values ("Bohemian Rhapsody", 160,"Lorem ipsum just Bohemian Rhapsody things", "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/bohemian-rhapsody-web.jpg",false);
-insert into movie(title, duration, description, image,is_showing )
-values ("Avengers", 160,"Lorem ipsum just avengers  things", "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543",false);
+"The 1970s was a golden age for brutal horror, thanks to the nixing of Hollywood's standard censorship code in the late 1960s. One of the most memorable posters from this era read Who will survive...and what will be left of them? With minimal gore and lots of twisted imagination, the original Texas Chain Saw holds up as a blisteringly freaky watch.",
+
+"https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_760/MTkwNTgxMjk3MDYzNTM1NzQx/the-texas-chain-saw-massacre.webp",
+
+true );
 
 
-insert into details_mov_and_cin(movie_id, cinema_id, price, schedule, occupied_seats)
-values (1, 1, 240, '2023-09-04 10:30:00', 'A1,A2');
-insert into details_mov_and_cin(movie_id, cinema_id, price, schedule, occupied_seats)
-values (2, 2, 240, '2023-09-04 13:30:00', 'B2,B3,C10');
-insert into details_mov_and_cin(movie_id, cinema_id, price, schedule, occupied_seats)
-values (3, 3, 1000, '2023-06-04 15:30:12', 'C5');
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("Parasite (2017)",
+
+160,
+
+"The history-making South Korean thriller that swept the Oscars had a brilliant poster that teased one of the film's many themes—identity, and the loss of it.  The image tells us nothing about the twisted plot while sucking us into its gravitational pull.",
+
+"https://e1.pxfuel.com/desktop-wallpaper/678/67/desktop-wallpaper-parasite-director-joon-parasite-movie.jpg", true );
+
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("Big George Foreman (2023)",
+
+140,
+
+"During the 1990s, George Foreman shocked the boxing world by becoming the oldest world champion nearly two decades after he had lost the title. Big George Foreman finally gives the legendary boxer his due with a biopic that chronicles his life story, from living in poverty to Olympic gold and an unmatched career in the ring.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/fdVd6thTstt0MQ4dUC1IXiOXpxv.jpg?p=1#038;p=1.jpg",
+
+true );
+
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("The Big Short",
+
+175,
+
+"Have you ever looked at Wall Street and thought that the entire thing is rigged against the little guy? If so, you’re probably not wrong. Director Adam McKay’s The Big Short examines the financial crash of 2007-2008 as investors like Michael Burry (Christian Bale), Mark Baum (Steve Carell), Jared Vennett (Ryan Gosling), and Ben Rickert (Brad Pitt) recognize the impending crisis, and manage to make millions off of it.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/isuQWbJPbjybBEWdcCaBUPmU0XO.jpg?p=1#038;p=1.jpg", true );
+
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("You Are So Not Invited to My Bat Mitzvah",
+
+120,
+
+"Adam Sandler produced and co-stars in the new Netflix comedy You Are So Not Invited To My Bat Mitzvah. However, Sandler only has a supporting role as Danny Friedman, the father of Stacy Friedman (Sunny Sandler). Stacy and her BFF, Lydia Rodriguez Katz (Samantha Lorraine), are in the middle of planning their epic Bat Mitzvahs when someone comes between them.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/ukpifWBW2xEmMtJX4bCpoNpWEr2.jpg?p=1#038;p=1.jpg", true );
+
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("Killer Book Club (2023)",
+
+85,
+
+"Do you like scary movies? That question tends to be associated with the Scream franchise, but it’s also appropriate for Netflix’s new Spanish horror film, Killer Book Club. In this film, eight friends share a love of horror movies, as well as a dark secret that is related to a fatal accident that took place in the past.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/zZH0sAUV0nfEEDBUdWBMLjs2XnN.jpg?p=1#038;p=1.jpg", false );
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("The Pope's Exorcist",
+
+90,
+
+"The Pope’s Exorcist is based on the books by Father Gabriele Amorth, a man who has been called “the James Bond of exorcists.” Russell Crowe headlines the film as Father Amorth, a skilled exorcist who manages to make enemies within the Vatican while taking on demons around the world.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/gNPqcv1tAifbN7PRNgqpzY8sEJZ.jpg?p=1#038;p=1.jpg", false );
+
+
+insert into movie(title, duration, description, image, is_showing )
+
+values ("Safe 2012",
+
+85,
+
+"If you’re tired of seeing Jason Statham fighting giant sharks in Meg 2, then Safe may be the movie that you’re looking for. In this 2012 action thriller, Statham plays Luke Wright, a former cop turned MMA fighter whose life becomes a nightmare when he accidentally crosses the Russian mafia by winning a fixed fight.",
+
+"https://www.digitaltrends.com/wp-content/uploads/2023/08/kOCpkoMUVae9UIf85gO71SyjLbW.jpg?p=1#038;p=1.jpg", false );
+
+
+
+
 
 insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
-values (4, 4, 1000, '2023-06-04 15:30:12');
+values (1, 1, 240, '2023-09-10 11:00:00');
 
 insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
-values (5, 5, 1000, '2023-06-04 15:30:12');
+values (2, 1, 180, '2023-09-10 13:30:00');
 
 insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
-values (6, 6, 1000, '2023-06-04 15:30:12');
+values (7, 1, 200, '2023-09-10 15:30:00');
 
 insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
-values (2, 6, 1000, '2023-06-04 15:30:12');
+values (4, 1, 160, '2023-09-10 17:30:00');
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (9, 2, 400, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (2, 2, 340, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (6, 2, 120, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 2, 450, '2023-09-10 17:30:00');
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (10, 3, 270, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (13, 3, 400, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (1, 3, 220, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (14, 3, 345, '2023-09-10 17:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (6, 3, 540, '2023-09-10 19:30:00');
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 4, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (14, 4, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 4, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (6, 4, 230, '2023-09-10 17:30:00');
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 4, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (12, 4, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (13, 4, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (6, 4, 230, '2023-09-10 17:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 4, 540, '2023-09-10 19:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (11, 4, 230, '2023-09-10 21:30:00');
+
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (8, 5, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (12, 5, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (1, 5, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (9, 5, 230, '2023-09-10 17:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 5, 540, '2023-09-10 19:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (11, 5, 230, '2023-09-10 21:30:00');
+
+
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (14, 6, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (1, 6, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 6, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (10, 6, 230, '2023-09-10 17:30:00');
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 7, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (1, 7, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 7, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (4, 7, 230, '2023-09-10 17:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 7, 540, '2023-09-10 19:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (11, 7, 230, '2023-09-10 21:30:00');
+
+
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (5, 8, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (2, 8, 210, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (9, 8, 210, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (10, 8, 230, '2023-09-10 17:30:00');
+
+
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (2, 9, 370, '2023-09-10 11:00:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (10, 9, 490, '2023-09-10 13:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (7, 9, 540, '2023-09-10 15:30:00');
+
+insert into details_mov_and_cin(movie_id, cinema_id, price, schedule )
+values (11, 9, 230, '2023-09-10 17:30:00');
 
